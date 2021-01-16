f9c40e781386300d40d13f0b6bc5513b676de24c
tallninja <tallninja123@gmail.com>
2021-01-16 14:52:38 +0300

    initial commit

---
+++ circuits_lab2/main.m
@@ -0,0 +1,5 @@
+function main()
+  for freq = linspace(50000 , 200000 , 16)
+    transferFunction(freq)
+  endfor 
+endfunction
---
+++ circuits_lab2/transferFunction.m
@@ -0,0 +1,5 @@
+function transferFunction(f)
+  mag = (2 * pi * f * 270e3) / sqrt(((-4 * pi^2 * f^2) + (3.333e11) )^2 + (2 * pi * f * 270e3)^2)
+endfunction
+
+ 
